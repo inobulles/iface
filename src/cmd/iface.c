478b83d99317e9c5d8aadc8a880ce8607cb83f94
obiwac <obiwac@gmail.com>
2023-01-31 00:27:28 +0100

    iface: Print out interface family

--- src/cmd/iface.c
+++ src/cmd/iface.c
@@ -17,7 +17,7 @@ static void usage(void) {
 }
 
 typedef struct {
-	iface_t* ifaces;
+	iface_t** ifaces;
 	size_t iface_count;
 
 	// this is the interface we're operating on
@@ -25,21 +25,13 @@ typedef struct {
 	iface_t* iface;
 } opts_t;
 
-static char* strkind(iface_kind_t kind) {
-	if (kind == IFACE_KIND_LOOP) return "loop";
-	if (kind == IFACE_KIND_ETH ) return "eth";
-	if (kind == IFACE_KIND_USB ) return "usb";
-
-	return "other";
-}
-
 static iface_t* search_iface(opts_t* opts, char const* name) {
 	if (!name) {
 		usage();
 	}
 
 	for (size_t i = 0; i < opts->iface_count; i++) {
-		iface_t* const iface = &opts->ifaces[i];
+		iface_t* const iface = opts->ifaces[i];
 
 		if (!strcmp(iface->name, name)) {
 			return iface;
@@ -59,7 +51,7 @@ static int do_enable(opts_t* opts) {
 		errx(EXIT_FAILURE, "iface_get_flags('%s'): %s", iface->name, iface_err_str());
 	}
 
-	iface->flags |= IFACE_UP;
+	iface->flags |= IFACE_FLAG_ONLINE;
 
 	if (iface_set_flags(iface) < 0) {
 		errx(EXIT_FAILURE, "iface_set_flags('%s'): %s", iface->name, iface_err_str());
@@ -75,7 +67,7 @@ static int do_disable(opts_t* opts) {
 		errx(EXIT_FAILURE, "iface_get_flags('%s'): %s", iface->name, iface_err_str());
 	}
 
-	iface->flags &= ~IFACE_UP;
+	iface->flags &= ~IFACE_FLAG_ONLINE;
 
 	if (iface_set_flags(iface) < 0) {
 		errx(EXIT_FAILURE, "iface_set_flags('%s'): %s", iface->name, iface_err_str());
@@ -86,7 +78,7 @@ static int do_disable(opts_t* opts) {
 
 static int do_list(opts_t* opts) {
 	for (size_t i = 0; i < opts->iface_count; i++) {
-		iface_t* const iface = &opts->ifaces[i];
+		iface_t* const iface = opts->ifaces[i];
 		char* ip = "n/a";
 
 		if (!iface_get_ip(iface)) {
@@ -95,7 +87,11 @@ static int do_list(opts_t* opts) {
 
 		iface_get_flags(iface);
 
-		printf("%s (%s)\t%s\t%s\n", iface->name, strkind(iface->kind), iface->flags & IFACE_UP ? "ONLINE" : "OFFLINE", ip);
+		printf("%s (%s)\t%s\t%s (%s)\n",
+			iface->name, iface_str_kind(iface->kind),
+			iface->flags & IFACE_FLAG_ONLINE ? "ONLINE" : "OFFLINE",
+			ip, iface_str_family(iface->family)
+		);
 	}
 
 	return 0;
@@ -138,8 +134,9 @@ int main(int argc, char* argv[]) {
 		char* const instr = *argv++;
 		int rv = EXIT_FAILURE; // I'm a pessimist
 
-		if (!strcmp(instr, "list")) {
-			rv = do_list(&opts);
+		if (!strcmp(instr, "disable")) {
+			opts.iface = (argc--, search_iface(&opts, *argv++));
+			rv = do_disable(&opts);
 		}
 
 		else if (!strcmp(instr, "enable")) {
@@ -147,9 +144,8 @@ int main(int argc, char* argv[]) {
 			rv = do_enable(&opts);
 		}
 
-		else if (!strcmp(instr, "disable")) {
-			opts.iface = (argc--, search_iface(&opts, *argv++));
-			rv = do_disable(&opts);
+		else if (!strcmp(instr, "list")) {
+			rv = do_list(&opts);
 		}
 
 		else {
